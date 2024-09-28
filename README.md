59b52853f79e7f2c45b79f375cfe608bddf5584b
elverarroyave <arroyave.elver@gmail.com>
2024-09-28 11:44:40 -0500

    Practica html css y js, ingeniería web

---
+++ README.md
@@ -0,0 +1,31 @@
+# Taller de seguimiento número 1
+
+## Presentado por:
+Elver Andres Arroyave
+
+## Curso:
+Ingeniería Web
+
+## Manual de uso Calculadora 
+Operaciones posibles en la calculadora
+
+ - Suma
+ - Resta
+ - Multiplicación
+ - División
+
+## Indicaciones de operación
+ La calculadora esta diseñada para realizar las operaciones que se mencionaron anteriormente, 
+ estas operasciones solo pueden ser ejecutadas entre dos números. 
+
+ Por ejemplo: 
+  
+   - 23+5
+   - 3*9
+   - 23/4
+
+Para realizar una operación se debe primero seleccionar el primer número que se desea operar, luego selecionar el tipo de operación que se quiere realizar, para posteriormente seleccionar el siguiente número a operar. Luego de esto se debe presionar el botón de igual para obtener el resultado procesado.
+
+## Detalles
+Proyecto de practica realizado con html. css y javascript
+
---
+++ script.js
@@ -0,0 +1,144 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+  
+    // Metodo para limpiar los valores
+    clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
+    }
+  
+    // Metodo para eliminar el ultimo valor
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+  
+    // Metodo para agregar un numero
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+  
+    // Metodo para elegir la operacion
+    chooseOperation(operation) {
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+    }
+  
+    // Metodo para realizar la operacion
+    compute() {
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+          computation = prev - current
+          break
+        case '*':
+          computation = prev * current
+          break
+        case '÷':
+          computation = prev / current
+          break
+        default:
+          return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
+    }
+  
+
+    // Metodo para mostrar los numeros
+    getDisplayNumber(number) {
+      const stringNumber = number.toString()
+      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const decimalDigits = stringNumber.split('.')[1]
+      let integerDisplay
+      if (isNaN(integerDigits)) {
+        integerDisplay = ''
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      }
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+      } else {
+        return integerDisplay
+      }
+    }
+  
+    // Metodo para actualizar la pantalla
+    updateDisplay() {
+      this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.innerText = ''
+      }
+    }
+  }
+  
+  
+  // Variables obtenidas de DOM
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+  // Eventos
+
+  //Evento para agregar un numero
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      console.log(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+
+  //Evento para elegir la operacion
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  // Evento para realizar la operacion
+  equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+  
+  // Evento para limpiar los valores
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+  
+  // Evento para eliminar el ultimo valor
+  deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
